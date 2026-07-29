2cd99e5681c2ecc2fe4fbac4c3e813a6ea0ce267
AVINASH V S <134091287+Avinash-vs@users.noreply.github.com>
2026-07-29 13:11:49 +0530

    Update S_Repository.java

--- CRUD/CRUD/src/main/java/com/example/crud/repository/S_Repository.java
+++ CRUD/CRUD/src/main/java/com/example/crud/repository/S_Repository.java
@@ -10,4 +10,13 @@ import com.example.crud.student.Student;
 public interface S_Repository extends JpaRepository<Student, Integer>{
 
 	List<Student> findByIdBetween(Integer startId, Integer endId);
+		@Query("SELECT s FROM Student s WHERE s.id BETWEEN :startId AND :endId")
+    List<Student> findStudentsBetweenIdsCustom(
+        @Param("startId") Integer startId,   
+        @Param("endId") Integer endId);
+
+	List<Student> findByIdLessThan(Integer id);
+	
+	@Query("SELECT s FROM Student s WHERE s.id < :Id")
+	List<Student> findStudentLessThanIdCustomList(@Param("Id") Integer Id);
 }
